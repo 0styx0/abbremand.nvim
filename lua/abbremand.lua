927a7d9ccfb11c779d6852cc4e4ce09fd786d67f
Styx Meiseles <git@0styx0.xyz>
2022-01-14 11:11:30 -0500

    refactor: reordered functions, export at eof

--- lua/abbremand.lua
+++ lua/abbremand.lua
@@ -4,23 +4,22 @@ local hooks = require('abbremand.hooks')
 -- note: nk = non-keyword (which can expand abbrevations. but can also be part of abbreviation values)
 -- functions exposed for unit tests prefixed with _. else local, or part of `abbremand`
 local abbremand = {
-    _keylogger = '',
-    _backspace_data = {
+    keylogger = '',
+    backspace_data = {
         consecutive_backspaces = 0,
         saved_keylogger = '',
         potential_trigger = '',
     },
-    _clients = {
+    clients = {
         forgotten = {},
         remembered = {},
         on_change = {},
     },
-    _enabled = false,
+    enabled = false,
 }
 
-function abbremand.clear_keylogger()
-    -- doing this on bufread fixes bug where characters C> are part of keylogger string
-    abbremand._keylogger = ''
+local function clear_keylogger()
+    abbremand.keylogger = ''
 end
 
 -- @Summary tracks backspacing. more complex than logic might initially seem
@@ -28,41 +27,136 @@ end
 --   so must differentiate between user vs expansion backspacing
 local function handle_backspacing(backspace_typed)
     if backspace_typed then
-        if abbremand._backspace_data.consecutive_backspaces == 0 then
-            abbremand._backspace_data.saved_keylogger = abbremand._keylogger
-            abbremand._backspace_data.potential_trigger = ''
+        if abbremand.backspace_data.consecutive_backspaces == 0 then
+            abbremand.backspace_data.saved_keylogger = abbremand.keylogger
+            abbremand.backspace_data.potential_trigger = ''
         end
 
-        abbremand._keylogger = abbremand._keylogger:sub(1, -2)
-        abbremand._backspace_data.consecutive_backspaces = abbremand._backspace_data.consecutive_backspaces + 1
+        abbremand.keylogger = abbremand.keylogger:sub(1, -2)
+        abbremand.backspace_data.consecutive_backspaces = abbremand.backspace_data.consecutive_backspaces + 1
         return
     end
 
-    if abbremand._backspace_data.consecutive_backspaces == 0 then
+    if abbremand.backspace_data.consecutive_backspaces == 0 then
         return
     end
 
     -- when abbr expanded, it deletes the trigger
     -- so later in @see check_abbrev_remembered, compare with actual trigger
-    abbremand._backspace_data.potential_trigger = string.sub(
-        abbremand._backspace_data.saved_keylogger,
-        #abbremand._backspace_data.saved_keylogger - abbremand._backspace_data.consecutive_backspaces + 1
+    abbremand.backspace_data.potential_trigger = string.sub(
+        abbremand.backspace_data.saved_keylogger,
+        #abbremand.backspace_data.saved_keylogger - abbremand.backspace_data.consecutive_backspaces + 1
     )
 
-    abbremand._backspace_data.consecutive_backspaces = 0
-    abbremand._backspace_data.saved_keylogger = ''
+    abbremand.backspace_data.consecutive_backspaces = 0
+    abbremand.backspace_data.saved_keylogger = ''
 end
 
 -- @return {boolean} if anything is using the plugin
 local function has_subscribers()
-    local clients = abbremand._clients
+    local clients = abbremand.clients
     return vim.tbl_count(clients.forgotten) > 0 or vim.tbl_count(clients.remembered) > 0
 end
 
-function abbremand.start()
+-- @return value if val_after_nk points to abbr value, else false
+local function contains_nk_abbr(text, val_after_nk)
+    local value_to_trigger, last_chunk_to_full_values = parse_abbrs.get_abbr_maps()
+    if not last_chunk_to_full_values[val_after_nk] then
+        return false
+    end
+
+    local potential_values = last_chunk_to_full_values[val_after_nk]
+
+    for _, value in ipairs(potential_values) do
+        if value_to_trigger[value] and string.find(text, value, #text - #value, true) then
+            return value
+        end
+    end
+
+    return false
+end
+
+-- @Summary checks if abbreviation functionality was used.
+--   if value was manually typed, notify user
+-- @return {-1, 0, 1} - if no abbreviation found (0), if user typed out the full value
+--   instead of using trigger (0), if it was triggered properly (1)
+local function check_abbrev_remembered(trigger, value, line_until_cursor)
+    local value_trigger = parse_abbrs.get_abbr_maps()
+
+    local abbr_exists = value_trigger[value] == trigger
+    if not abbr_exists then
+        return -1
+    end
+
+    local expanded_pat = vim.regex(trigger .. '[^[:keyword:]]' .. value)
+    local abbr_remembered = expanded_pat:match_str(abbremand.keylogger)
+
+    local expanded_midline_pat = vim.regex(trigger .. '[[:keyword:]]\\{' .. #trigger .. '}' .. value)
+    local abbr_remembered_midline = expanded_midline_pat:match_str(abbremand.keylogger)
+    local backspaced = trigger ~= '' and abbremand.backspace_data.potential_trigger == trigger
+    if abbr_remembered or backspaced or abbr_remembered_midline then
+        clear_keylogger()
+        hooks.trigger_callbacks(trigger, value, abbremand.clients, abbremand.clients.remembered)
+        abbremand.backspace_data.potential_trigger = ''
+        return 1
+    end
+
+    local forgotten_pat = vim.regex(value .. '[^[:keyword:]]')
+    local abbr_forgotten = forgotten_pat:match_str(line_until_cursor)
+
+    local val_in_logger = string.find(abbremand.keylogger, value, 1, true)
+
+    if abbr_forgotten and val_in_logger then
+        clear_keylogger()
+        hooks.trigger_callbacks(trigger, value, abbremand.clients, abbremand.clients.forgotten)
+        return 0
+    end
+
+    return -1
+end
+
+-- @Summary searches through what has been typed since the user last typed
+-- an abbreviation-expanding character, to see if an abbreviation has been used
+-- @return trigger, value. or -1 if not found
+local function find_abbrev(cur_char, line_until_cursor)
+    local keyword_regex = vim.regex('[[:keyword:]]')
+    local not_trigger_char = keyword_regex:match_str(cur_char)
+
+    if not_trigger_char then
+        return -1
+    end
+
+    local value_regex = vim.regex('[[:keyword:]]\\+[^[:keyword:]]\\+$')
+    local val_start, val_end = value_regex:match_str(line_until_cursor)
+    if not val_start then
+        return -1
+    end
+
+    val_start = val_start + 1
+    val_end = val_end - 1
+    local potential_value = line_until_cursor:sub(val_start, val_end)
+
+    local value_to_trigger = parse_abbrs.get_abbr_maps()
+    local potential_trigger = value_to_trigger[potential_value]
+
+    -- potential_value only contains characters after last non-keyword char
+    local nk_value = contains_nk_abbr(line_until_cursor, potential_value)
+    if nk_value then
+        local nk_trigger = value_to_trigger[nk_value]
+        check_abbrev_remembered(nk_trigger, nk_value, line_until_cursor)
+        return nk_trigger, nk_value
+    elseif potential_trigger then
+        check_abbrev_remembered(potential_trigger, potential_value, line_until_cursor)
+        return potential_trigger, potential_value
+    end
+
+    return -1
+end
+
+local function scan_for_abbrs()
     vim.api.nvim_buf_attach(0, false, {
 
-        on_detach = abbremand.clear_keylogger,
+        on_detach = clear_keylogger,
 
         on_bytes = function(
             byte_str,
@@ -89,14 +183,14 @@ function abbremand.start()
             if vim.api.nvim_get_mode().mode ~= 'i' then
                 -- allows for reminders to take into account normal mode changes
                 -- using nvim_get_current_line gives out of bounds error for some reason
-                abbremand._keylogger = vim.fn.getline('.')
+                abbremand.keylogger = vim.fn.getline('.')
                 return false
             end
 
             local line = vim.api.nvim_buf_get_lines(0, start_row, start_row + 1, true)[1]
 
             local cur_char = line:sub(start_col + 1, start_col + 1)
-            abbremand._keylogger = abbremand._keylogger .. cur_char
+            abbremand.keylogger = abbremand.keylogger .. cur_char
 
             local cursor_col = start_col + new_end_col
             local line_until_cursor = line:sub(0, cursor_col)
@@ -109,112 +203,17 @@ function abbremand.start()
             if user_backspaced then
                 handle_backspacing(true)
             else
-                abbremand._find_abbrev(cur_char, line_until_cursor)
+                find_abbrev(cur_char, line_until_cursor)
                 handle_backspacing(false)
             end
         end,
     })
 end
 
--- @return value if val_after_nk points to abbr value, else false
-function abbremand._contains_nk_abbr(text, val_after_nk)
-    local value_to_trigger, last_chunk_to_full_values = parse_abbrs.get_abbr_maps()
-    if not last_chunk_to_full_values[val_after_nk] then
-        return false
-    end
-
-    local potential_values = last_chunk_to_full_values[val_after_nk]
-
-    for _, value in ipairs(potential_values) do
-        if value_to_trigger[value] and string.find(text, value, #text - #value, true) then
-            return value
-        end
-    end
-
-    return false
-end
-
--- @Summary searches through what has been typed since the user last typed
--- an abbreviation-expanding character, to see if an abbreviation has been used
--- @return trigger, value. or -1 if not found
-function abbremand._find_abbrev(cur_char, line_until_cursor)
-    local keyword_regex = vim.regex('[[:keyword:]]')
-    local not_trigger_char = keyword_regex:match_str(cur_char)
-
-    if not_trigger_char then
-        return -1
-    end
-
-    local value_regex = vim.regex('[[:keyword:]]\\+[^[:keyword:]]\\+$')
-    local val_start, val_end = value_regex:match_str(line_until_cursor)
-    if not val_start then
-        return -1
-    end
-
-    val_start = val_start + 1
-    val_end = val_end - 1
-    local potential_value = line_until_cursor:sub(val_start, val_end)
-
-    local value_to_trigger = parse_abbrs.get_abbr_maps()
-    local potential_trigger = value_to_trigger[potential_value]
-
-    -- potential_value only contains characters after last non-keyword char
-    local nk_value = abbremand._contains_nk_abbr(line_until_cursor, potential_value)
-    if nk_value then
-        local nk_trigger = value_to_trigger[nk_value]
-        abbremand._check_abbrev_remembered(nk_trigger, nk_value, line_until_cursor)
-        return nk_trigger, nk_value
-    elseif potential_trigger then
-        abbremand._check_abbrev_remembered(potential_trigger, potential_value, line_until_cursor)
-        return potential_trigger, potential_value
-    end
-
-    return -1
-end
-
--- @Summary checks if abbreviation functionality was used.
---   if value was manually typed, notify user
--- @return {-1, 0, 1} - if no abbreviation found (0), if user typed out the full value
---   instead of using trigger (0), if it was triggered properly (1)
-function abbremand._check_abbrev_remembered(trigger, value, line_until_cursor)
-    local value_trigger = parse_abbrs.get_abbr_maps()
-    local abbr_exists = value_trigger[value] == trigger
-    if not abbr_exists then
-        return -1
-    end
-
-    local expanded_pat = vim.regex(trigger .. '[^[:keyword:]]' .. value)
-    local abbr_remembered = expanded_pat:match_str(abbremand._keylogger)
-
-    local expanded_midline_pat = vim.regex(trigger .. '[[:keyword:]]\\{' .. #trigger .. '}' .. value)
-    local abbr_remembered_midline = expanded_midline_pat:match_str(abbremand._keylogger)
-
-    if abbr_remembered or abbremand._backspace_data.potential_trigger == trigger or abbr_remembered_midline then
-        abbremand.clear_keylogger()
-        hooks.trigger_callbacks(trigger, value, abbremand._clients, abbremand._clients.remembered)
-        abbremand._backspace_data.potential_trigger = ''
-        return 1
-    end
-
-    local forgotten_pat = vim.regex(value .. '[^[:keyword:]]')
-    local abbr_forgotten = forgotten_pat:match_str(line_until_cursor)
-
-    local val_in_logger = string.find(abbremand._keylogger, value, 1, true)
-
-    if abbr_forgotten and val_in_logger then
-        abbremand.clear_keylogger()
-        hooks.trigger_callbacks(trigger, value, abbremand._clients, abbremand._clients.forgotten)
-        return 0
-    end
-
-    return -1
-end
-
-function abbremand.disable()
-    -- setting this makes `nvim_buf_attach` return true,
-    -- detaching from buffer and clearing keylogger
-    -- @see abbremand.start
-    abbremand._enabled = false
+local function remove_autocmds()
+    vim.cmd([[
+    command! -bang AbbremandDisable autocmd! Abbremand
+    ]])
 end
 
 local function create_autocmds()
@@ -222,15 +221,32 @@ local function create_autocmds()
     vim.cmd[[
     augroup Abbremand
     autocmd!
-    autocmd TextChanged,TextChangedI * :lua require('abbremand.hooks').monitor_abbrs(require('abbremand')._clients)
+    autocmd TextChanged,TextChangedI * :lua require('abbremand').handle_on_change()
     augroup END
     ]]
 end
 
-function abbremand.enable()
-    abbremand.start()
+local function handle_on_change()
+
+    if vim.tbl_count(abbremand.clients.on_change) < 0 then
+        remove_autocmds()
+        return
+    end
+
+    hooks.monitor_abbrs(abbremand.clients)
+end
+
+local function disable()
+    -- setting this makes `nvim_buf_attach` return true,
+    -- detaching from buffer and clearing keylogger
+    -- @see abbremand.start
+    abbremand.enabled = false
+end
+
+local function enable()
+    scan_for_abbrs()
     create_autocmds()
-    abbremand._enabled = true
+    abbremand.enabled = true
 end
 
 -- @param callback: function which will receive as arguments:
@@ -238,11 +254,11 @@ end
 --   as arguments when abbreviation was forgotten
 --   on_change will be fired if value is modified later
 -- If callback returns `false` it is unsubscribed from future forgotten events
-function abbremand.on_abbr_forgotten(callback)
-    if not abbremand._enabled then
-        abbremand.enable()
+local function on_abbr_forgotten(callback)
+    if not abbremand.enabled then
+        enable()
     end
-    table.insert(abbremand._clients.forgotten, callback)
+    table.insert(abbremand.clients.forgotten, callback)
 end
 
 -- @param callback: function which will receive as arguments:
@@ -250,11 +266,26 @@ end
 --   as arguments when abbreviation was expanded
 --   on_change will be fired if value is modified later
 -- If callback returns `false` it is unsubscribed from future remembered events
-function abbremand.on_abbr_remembered(callback)
-    if not abbremand._enabled then
-        abbremand.enable()
+local function on_abbr_remembered(callback)
+    if not abbremand.enabled then
+        enable()
     end
-    table.insert(abbremand._clients.remembered, callback)
+    table.insert(abbremand.clients.remembered, callback)
 end
 
-return abbremand
+return {
+    enable = enable,
+    disable = disable,
+    scan_for_abbrs = scan_for_abbrs,
+    on_abbr_remembered = on_abbr_remembered,
+    on_abbr_forgotten = on_abbr_forgotten,
+    handle_on_change = handle_on_change,
+    _contains_nk_abbr = contains_nk_abbr,
+    _find_abbrev = find_abbrev,
+    _check_abbrev_remembered = check_abbrev_remembered,
+    _set_keylogger = function(value)
+        abbremand.keylogger = value
+        return value
+    end
+}
+
--- test/plenary/check_abbrev_remembered_spec.lua
+++ test/plenary/check_abbrev_remembered_spec.lua
@@ -1,8 +1,9 @@
 local assert = require('luassert.assert')
 local stub = require('luassert.stub')
-local abbremand = require('abbremand')
 local helpers = require('test.plenary.helpers')
 local spy = require('luassert.spy')
+local abbremand = require('abbremand')
+local parse_abbrs = require('abbremand.parse_abbrs')
 
 describe('check_abbrev_remembered', function()
     local trigger = helpers.abbrs.generic[1].trigger
@@ -13,7 +14,7 @@ describe('check_abbrev_remembered', function()
     local keyword, non_keyword = helpers.set_keyword()
     local spied_callback;
 
-    stub(abbremand, '_get_abbrevs_val_trigger').returns({ [value] = trigger })
+    stub(parse_abbrs, 'get_abbr_maps').returns({ [value] = trigger })
     stub(vim.api, 'nvim_buf_set_extmark').returns('check_abbrev_remembered_spec_stubbed')
 
     before_each(function()
@@ -26,49 +27,49 @@ describe('check_abbrev_remembered', function()
     end)
 
     it('identifies when an abbreviation _was_ expanded', function()
-        abbremand._keylogger = trigger .. non_keyword .. value
+        local text = abbremand._set_keylogger(trigger .. non_keyword .. value)
 
-        local remembered = abbremand._check_abbrev_remembered(trigger, value, abbremand._keylogger)
+        local remembered = abbremand._check_abbrev_remembered(trigger, value, text)
         assert.are.same(1, remembered)
         assert.spy(spied_callback).was.Not.called()
     end)
 
     it('identifies when an abbreviation was _not_ expanded', function()
-        abbremand._keylogger = 'random no trigger stuff ' .. value .. non_keyword
+        local text = abbremand._set_keylogger('random no trigger stuff ' .. value .. non_keyword)
 
-        local remembered = abbremand._check_abbrev_remembered(trigger, value, abbremand._keylogger)
+        local remembered = abbremand._check_abbrev_remembered(trigger, value, text)
         assert.are.same(0, remembered)
         assert.spy(spied_callback).was.called(1)
     end)
 
     it('identifies when something is _not_ a potential abbreviation', function()
-        abbremand._keylogger = value .. keyword
+        local text = abbremand._set_keylogger(value .. keyword)
 
-        local remembered = abbremand._check_abbrev_remembered(trigger, value, abbremand._keylogger)
+        local remembered = abbremand._check_abbrev_remembered(trigger, value, text)
         assert.are.same(-1, remembered)
         assert.spy(spied_callback).was.Not.called()
     end)
 
     describe('identifies correctly if called twice in a row and', function()
         it('first time expanded, second not expanded', function()
-            abbremand._keylogger = trigger .. non_keyword .. value .. non_keyword
+            local first_text = abbremand._set_keylogger(trigger .. non_keyword .. value .. non_keyword)
 
-            local remembered = abbremand._check_abbrev_remembered(trigger, value, abbremand._keylogger)
+            local remembered = abbremand._check_abbrev_remembered(trigger, value, first_text)
             assert.are.same(1, remembered)
 
-            abbremand._keylogger = abbremand._keylogger .. value .. non_keyword
-            local remembered_second = abbremand._check_abbrev_remembered(trigger, value, abbremand._keylogger)
+            local second_text = abbremand._set_keylogger(value .. non_keyword)
+            local remembered_second = abbremand._check_abbrev_remembered(trigger, value, second_text)
             assert.are.same(0, remembered_second)
         end)
 
         it('first time not an abbreviation, second not expanded', function()
-            abbremand._keylogger = value .. keyword
+            local first_text = abbremand._set_keylogger(value .. keyword)
 
-            local remembered = abbremand._check_abbrev_remembered(trigger, value, abbremand._keylogger)
+            local remembered = abbremand._check_abbrev_remembered(trigger, value, first_text)
             assert.are.same(-1, remembered)
 
-            abbremand._keylogger = abbremand._keylogger .. value .. non_keyword
-            local remembered_second = abbremand._check_abbrev_remembered(trigger, value, abbremand._keylogger)
+            local second_text = abbremand._set_keylogger(first_text .. value .. non_keyword)
+            local remembered_second = abbremand._check_abbrev_remembered(trigger, value, second_text)
             assert.are.same(0, remembered_second)
         end)
     end)
